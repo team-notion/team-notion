5353fe32264e2c6bacf2c50e1116bbad1e210aa4
BrotherlyHamlet16 <80749000+Clever-Niwagaba@users.noreply.github.com>
2025-09-25 14:48:08 +0300

    car inventory module

--- backend/app/Models/Car.php
+++ backend/app/Models/Car.php
@@ -6,5 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class Car extends Model
 {
-    //
+    protected $fillable = [
+        'owner_id',
+        'make',
+        'model',
+        'type',
+        'price_per_day',
+        'availability_start',
+        'availability_end',
+        'rules'
+    ];
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class, 'owner_id');
+    }
+
+    public function photos()
+    {
+        return $this->hasMany(CarPhoto::class);
+    }
+
+    public function reservations()
+    {
+        return $this->hasMany(Reservation::class);
+    }
 }
