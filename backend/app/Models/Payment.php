6c068d65cf4525657f7ef84e73265d6496ac82ff
BrotherlyHamlet16 <80749000+Clever-Niwagaba@users.noreply.github.com>
2025-09-25 14:55:49 +0300

    payment module

--- backend/app/Models/Payment.php
+++ backend/app/Models/Payment.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
-    //
+    protected $fillable = [
+        'reservation_id',
+        'payment_method_id',
+        'amount',
+        'currency',
+        'status',
+        'transaction_reference'
+    ];
+
+    public function reservation()
+    {
+        return $this->belongsTo(Reservation::class);
+    }
+
+    public function paymentMethod()
+    {
+        return $this->belongsTo(PaymentMethod::class);
+    }
 }
