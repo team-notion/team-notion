0b3b0c4d7bf1331053b682eedc3cc0e4d890bf30
BrotherlyHamlet16 <80749000+Clever-Niwagaba@users.noreply.github.com>
2025-09-25 15:02:47 +0300

    notification module

--- backend/app/Models/Notification.php
+++ backend/app/Models/Notification.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Notification extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'reservation_id',
+        'channel',
+        'message',
+        'status'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function reservation()
+    {
+        return $this->belongsTo(Reservation::class);
+    }
 }
---
+++ backend/database/migrations/2025_09_25_115738_create_notifications_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable()->constrained()->cascadeOnDelete();
+            $table->foreignId('reservation_id')->nullable()->constrained()->cascadeOnDelete();
+            $table->enum('channel', ['email', 'sms', 'push']);
+            $table->text('message');
+            $table->enum('status', ['sent', 'pending', 'failed']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notifications');
+    }
+};
