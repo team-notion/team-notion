779774566edcb3c5e9bed4f76b522a340246e8a9
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2025-10-20 13:37:07 +0100

    add fallback for car photos in car serializers

--- backend/django/settings/base.py
+++ backend/django/settings/base.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    "cloudinary",
+    "cloudinary_storage",
     'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
