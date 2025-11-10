1367d6e30e3a3d5ef190b95032ca4482918b9311
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2025-11-10 13:05:21 +0100

    revert email service to default SMTP

---
+++ backend/django/notion_rides/settings/dev.py
@@ -0,0 +1,9 @@
+from .base import *
+
+DEBUG = True
+ALLOWED_HOSTS = ['*']
+CORS_ALLOW_ALL_ORIGINS = True
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
---
+++ backend/django/notion_rides/settings/prod.py
@@ -0,0 +1,34 @@
+from .base import *
+import os
+
+DEBUG = False
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+
+frontend_urls = os.getenv("FRONTEND_URLS", "")
+if frontend_urls:
+    CORS_ALLOWED_ORIGINS = [url.strip() for url in frontend_urls.split(",")]
+else:
+    CORS_ALLOW_ALL_ORIGINS = True 
+
+CORS_ALLOW_CREDENTIALS = True
+
+from corsheaders.defaults import default_headers
+
+CORS_ALLOW_HEADERS = list(default_headers) + [
+    "secureddata",
+]
+
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+#INSTALLED_APPS += ['cloudinary', 'cloudinary_storage']
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+}
