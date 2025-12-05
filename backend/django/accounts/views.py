7e44d514e38cf84cfa0d8f722556e192b0657992
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2025-12-05 11:13:03 +0100

    revert to threads

--- backend/django/accounts/views.py
+++ backend/django/accounts/views.py
@@ -202,8 +202,8 @@ class RequestPasswordResetView(APIView):
         
         reset_link = f"{frontend_url}/reset-password/{uid}/{token}/"
         
-        #Thread(target=send_password_reset_email, args=(user, reset_link)).start()
-        send_password_reset_email_task.delay(user.id, reset_link)
+        Thread(target=send_password_reset_email, args=(user, reset_link)).start()
+        #send_password_reset_email_task.delay(user.id, reset_link)
 
         return Response({'message': 'Password reset email sent'}, status=status.HTTP_200_OK)
 
